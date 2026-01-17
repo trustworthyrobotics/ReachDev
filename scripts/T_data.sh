e2601294da8207cfd6dfaad6108a1b6ae981d21d
keyis2 <keyis2@illinois.edu>
2026-01-16 21:31:34 -0600

    wrap up

---
+++ scripts/T_data.sh
@@ -0,0 +1,11 @@
+python data/T_pushing/generate.py settings.data_mode='dt_dyn' data.training=True
+
+python data/T_pushing/generate.py settings.data_mode='dt_dyn' data.training=False
+
+python data/T_pushing/generate.py settings.data_mode='ct_dyn' data.training=True
+
+python data/T_pushing/generate.py settings.data_mode='ct_dyn' data.training=False
+
+python data/T_pushing/generate.py settings.data_mode='ct_ctl' data.training=True
+
+python data/T_pushing/generate.py settings.data_mode='ct_ctl' data.training=False
--- scripts/T_train.sh
+++ scripts/T_train.sh
@@ -1,3 +1,5 @@
 python train.py --config-name quadrotor settings.train_mode='dt_dyn'
 
+python train.py --config-name quadrotor settings.train_mode='ct_dyn'
+
 python train.py --config-name quadrotor settings.train_mode='ct_ctl'
